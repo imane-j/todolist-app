3851aa6f54eb9578a6d3353ada410ab1ad34c816
Imane Jarmouni <jarmounimane@gmail.com>
2019-09-20 18:55:45 +0200

    Application en React

---
+++ precache-manifest.4acf43c0300bcce7b264d9780d1eb7af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1929a844611164f84ca69a8c85729b29",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9628e80ea828cad6f125",
+    "url": "/todolist/static/css/main.8e4b8a05.chunk.css"
+  },
+  {
+    "revision": "1db87aba7786202b2f03",
+    "url": "/todolist/static/js/2.6b7226e4.chunk.js"
+  },
+  {
+    "revision": "9628e80ea828cad6f125",
+    "url": "/todolist/static/js/main.565b4181.chunk.js"
+  },
+  {
+    "revision": "9208d2de4adbe8fe7ce1",
+    "url": "/todolist/static/js/runtime~main.394f96a6.js"
+  },
+  {
+    "revision": "1c91d5d0a051e71b565b91f91a41d9cd",
+    "url": "/todolist/static/media/confetti.1c91d5d0.svg"
+  },
+  {
+    "revision": "8e665ca81135bba9ec861a295df2359a",
+    "url": "/todolist/static/media/plus.8e665ca8.svg"
+  }
+]);
